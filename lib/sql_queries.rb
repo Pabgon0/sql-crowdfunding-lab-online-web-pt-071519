df44893817d59e15692fdcee5998e471de627d11
Pablo Gonzalez <pabgon0@gmail.com>
2019-09-25 18:59:09 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,6 +27,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   FROM projects
   JOIN pledges ON pledges.project_id = projects.id
   GROUP BY project_id
+  WHERE funding_goal < SUM(pledges.amount)
   ORDER BY title
   DESC"
 end
