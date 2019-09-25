73c49e9673d279e8dfa48ec9153858ad6fb2ac44
Pablo Gonzalez <pabgon0@gmail.com>
2019-09-25 18:08:47 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,6 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   FROM projects, pledges
   GROUP BY pledges.project_id, projects.title
   HAVING projects.id = pledges.project_id
+  ORDER BY
   ASC"
 end
 
